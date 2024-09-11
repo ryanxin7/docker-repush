0a9ef45ed0ed66a6e1cadf65a8fbc37ad810d6d0
xin <xx9z@outlook.com>
2024-09-11 10:16:31 +0800

    使用 JavaScript 的 window.location.host 来动态获取主机地址,解决兼容性问题

--- docker-build.sh
+++ docker-build.sh
@@ -4,13 +4,13 @@
 DATE=$(date +"%Y%m%d")
 
 # 设置镜像名称，带日期后缀
-IMAGE_NAME=repush:$DATE"
+IMAGE_NAME="repush:$DATE"
 
 # 输出构建信息
 echo "Building Docker image: $IMAGE_NAME"
 
 # 构建 Docker 镜像
-docker build -t $IMAGE_NAME .
+docker build -t "$IMAGE_NAME" .
 
 # 输出完成信息
 echo "Docker image $IMAGE_NAME built successfully"
